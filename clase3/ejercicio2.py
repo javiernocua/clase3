a16cb6edd5d7b355d44decdbac89e58807250a4c
javiernocua <83758126+javiernocua@users.noreply.github.com>
2021-05-16 18:18:34 -0500

    Delete clase3 directory

--- clase3/ejercicio2.py
+++
@@ -1,7 +0,0 @@
-#ejercicio 2
-peso_empaque=float(input("Ingrese peso de empaque: "))
-precio_empaque=float(input("Ingrese precio de empaque: "))
-peso_kilo= 1000
-precio_gramo= precio_empaque/peso_empaque
-precio_kilo= precio_gramo*peso_kilo
-print("el precio por kilo es:",precio_kilo)
